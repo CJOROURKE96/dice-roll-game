90761700cbaef1950e1035e1b2600c9c8240bef2
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-05-05 11:01:56 +0100

    add basic dice roll mechanics to build on

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+Console.WriteLine(roll1);
+Console.WriteLine(roll2);
+Console.WriteLine(roll3);
