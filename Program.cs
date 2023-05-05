dd229172de346d07945ec28acd64018f29e57b73
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-05-05 11:34:35 +0100

    add a total to count the dice rolls, then display that something has been won depedning on the total roll

--- Program.cs
+++ Program.cs
@@ -4,15 +4,28 @@ int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
-Console.WriteLine(roll1);
-Console.WriteLine(roll2);
-Console.WriteLine(roll3);
+int total = roll1 + roll2 + roll3;
+
 
 if ((roll1 == roll2)|| (roll2 == roll3) || (roll1 == roll3)) {
 if ((roll1 == roll2) && (roll2 == roll3)) {
-    Console.WriteLine("You rolled triples!");
+    Console.WriteLine("You rolled triples! +6 bonus to total");
+    total += 6;
 } else
     {
-    Console.WriteLine("You rolled doubles!");
+    Console.WriteLine("You rolled doubles! +2 bonus to total");
+    total += 2;
 }
+}
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total >= 16) {
+    Console.WriteLine("You win a new car!");
+} else if (total >= 10) {
+    Console.WriteLine("You win a new laptop!");
+} else if (total == 7) {
+    Console.WriteLine("You win a trip for two!");
+} else {
+    Console.WriteLine("You win a free meal!");
 }
