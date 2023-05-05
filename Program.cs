71d66b8581e3718cb2c39fd426cf04ee7bd5c8c2
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-05-05 16:55:34 +0100

    attempt to fix a merge issue

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ if (total >= 16) {
     Console.WriteLine("You win a new car!");
 } else if (total >= 10) {
     Console.WriteLine("You win a new laptop!");
-} else if (total >= 7) {
+} else if (total == 7) {
     Console.WriteLine("You win a trip for two!");
 } else {
     Console.WriteLine("You win a free meal!");
