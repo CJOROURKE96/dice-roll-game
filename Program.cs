4f29f19fbdb1851aaba50c430c35cee471bf4a1d
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-05-05 11:06:39 +0100

    add logic to check if you have rolled doubles or triples, then print that response to the console

--- Program.cs
+++ Program.cs
@@ -7,3 +7,12 @@ int roll3 = dice.Next(1, 7);
 Console.WriteLine(roll1);
 Console.WriteLine(roll2);
 Console.WriteLine(roll3);
+
+if ((roll1 == roll2)|| (roll2 == roll3) || (roll1 == roll3)) {
+if ((roll1 == roll2) && (roll2 == roll3)) {
+    Console.WriteLine("You rolled triples!");
+} else
+    {
+    Console.WriteLine("You rolled doubles!");
+}
+}
